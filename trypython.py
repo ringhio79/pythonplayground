89f09c6daf9054b73ad61a006c206d0fe1e30534
Giselle <gigi108@gmail.com>
2018-07-09 12:33:15 +0000

    half through questios

---
+++ trypython.py
@@ -0,0 +1,43 @@
+# print('Hello World')
+# print('Welcome To Python')
+
+# name = input("What is your name?")
+# print("hello " + name)
+
+# x = int(input("pick a number"))
+# y = int(input("pick another number"))
+# print(x + y)
+
+# string = input('please type something: ')
+# print(string.upper())
+
+# string = input('type in caps: ')
+# print(string.lower())
+
+# string = input("type something here")
+# print(string.title())
+
+# s = input("say something here")
+
+# str = ''
+# for i in s:
+#     str = i + str
+# print(str)
+
+# num1 = int(input('enter a number here '))
+
+# if num1 <= 10:
+#     print('small')
+# else:
+#     print('big')
+    
+num1 = int(input('enter a random number'))
+num2 = int(input('enter another number'))
+
+if num1 == num2:
+    print('same')
+else:
+    print('different')
+
+
+
