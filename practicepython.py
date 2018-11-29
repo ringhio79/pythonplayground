fea6025f0dfc5e45b2bce30e281fbcdc0182c204
Giselle <gigi108@gmail.com>
2018-11-29 13:40:48 +0000

    completed challenge 8, rock paper scissors

--- practicepython.py
+++ practicepython.py
@@ -53,16 +53,82 @@
 
 # challenge 4 Divisors
 
-num = int(input("Choose a number to find it's divisors: "))
-max_num = num+1
-div_by = range(2,max_num)
-divisors = []
+# num = int(input("Choose a number to find it's divisors: "))
+# max_num = num+1
+# div_by = range(2,max_num)
+# divisors = []
 
 
-for i in div_by:
-    if num % i == 0:
-        divisors.append(i)
-        print(divisors)
+# for i in div_by:
+#     if num % i == 0:
+#         divisors.append(i)
+# print(divisors)
+
+
+
+# # Challenge 5 List overlap
+
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# blended_list = []
+
+# for i in a:
+#     if i in b:
+#         if i not in blended_list:
+#             blended_list.append(i)
+# print (blended_list)
+
     
     
+# import random
+# a=[]
+# b=[]
+# blended_list = []
 
+# for x in range(10):
+#   a.append(random.randint(1,21))
+#   b.append(random.randint(1,21))
+  
+#   for i in a:
+#       if i in b:
+#           if i not in blended_list:
+#               blended_list.append(i)
+# print (a)
+# print (b)
+# print (blended_list)
+
+# Challenge 6
+
+# simon_says = input("Please write a word here: ")
+
+# if simon_says[::-1] == simon_says[0:]:
+#     print ("this is a palindrome")
+# else:
+#     print("this is not a palindrome")
+    
+    
+# Challenge 7
+
+# a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# even=[]
+# for i in a:
+#     if i %2==0:
+#         even.append(i)
+# print(even)
+
+# a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# even = [i for i in a if i%2== 0]
+# print(even)
+
+
+# Challenge 8 - Rock paper scissors
+
+player_1 = input("P1 choose your weapon (R=rock, P=paper or S=scissors)\n").upper()
+player_2 = input("P2 choose your weapon (R=rock, P=paper or S=scissors)\n").upper()
+
+if player_1 == player_2:
+    print ("No win, play again")
+if (player_1 == "R" and player_2 == "S") or ( player_1 == "S" and player_2 == "P") or ( player_1 == "P" and player_2 == "R"):
+    print ("Player 1 wins")
+else:
+    print ("Player 2 wins")
